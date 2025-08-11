f71e53ee177514a327866d59f83a3ed186b5e7ac
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2025-08-11 14:06:16 +0800

    Updated config.py
    Updated config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@
 
 # helper for local development
 import os
-os.environ.setdefault("DB_URL", "mysql+pymysql://root:password@localhost:3306/skylance")
+os.environ.setdefault("DB_URL", "mysql+pymysql://skylanceuser:strongpasswordd@146.190.97.82:3306/skylance")
 
 # pull it back out into a module-level variable
 DB_URL = os.environ["DB_URL"]
